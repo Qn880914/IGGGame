c57cb08217566c1ae304fc37128f431c1ac6ba2d
lyy19910615 <986837786@qq.com>
2019-06-11 20:47:04 +0800

    test

--- test.cs
+++ test.cs
@@ -8,21 +8,31 @@ public class test : MonoBehaviour
 
     private static System.Func<int, Player> analyzeCustomDepend;
 
-    Dictionary<uint, Player> m_Players = new Dictionary<uint, Player>();
+    Dictionary<string, Player> m_Players = new Dictionary<string, Player>();
     // Start is called before the first frame update
     void Start()
     {
-        m_Players.TryGetValue(1, out Player player);
+        m_Players.TryGetValue("1", out Player player);
         int k = 10;
         player = new Player { name = "mm"};
         player = analyzeCustomDepend?.Invoke(2);
         int i = 0;
+
+
+        m_Players.Add("0", new Player());
+        m_Players.Add("1", new Player());
+        m_Players.Add("2", new Player());
+        m_Players.Add("3", new Player());
+        m_Players.Add("4", new Player());
     }
 
     // Update is called once per frame
     void Update()
     {
-        UnityEngine.Debug.Log(aa.ToString("N2"));
+        //UnityEngine.Debug.Log(aa.ToString("N2"));
+
+
+        m_Players.TryGetValue("1", out Player player);
     }
 
     class Player
